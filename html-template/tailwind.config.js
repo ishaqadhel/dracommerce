cc4f64f87c725bfff4cc8074dd14537701352285
ishaqadhel <ishaq.adhel@gmail.com>
2021-12-04 01:11:54 +0700

    feat: add html-template landing page

---
+++ html-template/tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        cerise: {
+            50: "#fad1dd",
+            100: "#f5a3ba",
+            200: "#f07598",
+            300: "#ee5d86",
+            400: "#ec4675",
+            500: "#ea3266",
+            600: "#e71853",
+            700: "#d0164a",
+            800: "#b91342",
+            900: "#a2113a",
+        },
+    },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
